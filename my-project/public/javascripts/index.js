36754df0a9b87095130c672101d5467c02d6974a
Your Name <you@example.com>
2018-03-01 09:53:14 +0800

    完成登录

--- my-project/public/javascripts/index.js
+++
@@ -1,4 +0,0 @@
-$("#left-div>ul>li").click(function(){
-	$(this).parent().toggleClass("current");
-	$(this).parent().siblings().removeClass("current");
-});
---
+++ my-project/public/javascripts/shouye.js
@@ -0,0 +1,5 @@
+
+$("#left>ul>li>a").click(function(){
+	$(this).parent().toggleClass("current");
+	$(this).parent().siblings().removeClass("current");
+});
